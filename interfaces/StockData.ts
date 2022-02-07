c43aa4454a6942c5defdfca22df151b44a525f8c
Sergey Stadnik <sergey@snowytime.net>
2022-02-07 22:29:25 +1100

    feat: remove unused data fields

--- interfaces/StockData.ts
+++ interfaces/StockData.ts
@@ -3,12 +3,10 @@ export interface StockData {
     // Comments are sample data for Royal Bank of Canada
     canonical_url: string; // "/stocks/ca/banks/tsx-ry/royal-bank-of-canada-shares"
     company_id: string; // "1592FD9F-BF5B-469D-B8F3-D33379E0C0DE"
-    exchange_symbol: string; // "TSX"
     id: number; // 109809;
     info: {
       data: {
         description: string; // "Royal Bank of Canada operates as a diversified financial service company worldwide"
-        logo_url: string; // "/api/company/image/NYSE:RY/logo"
       };
     };
     name: string; // "Royal Bank of Canada"
@@ -25,7 +23,5 @@ export interface StockData {
         value: number; // 2
       };
     };
-    slug: string; // "royal-bank-of-canada"
-    ticker_symbol: string; // "RY"
     unique_symbol: string; // "TSX:RY"
   }
