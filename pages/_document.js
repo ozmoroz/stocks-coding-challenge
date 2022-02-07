8c4936f76093938d250cd23e800595e000053453
Sergey Stadnik <sergey@snowytime.net>
2022-02-07 16:34:29 +1100

    chore: add styled-components

---
+++ pages/_document.js
@@ -0,0 +1,53 @@
+// Inject styled-components into SSR
+// Ref: https://styled-components.com/docs/advanced#nextjs
+
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+
+  render() {
+    return (
+      // Add Roboto font for MUI
+      // Ref: https://nextjs.org/docs/basic-features/font-optimization
+      // https://mui.com/getting-started/installation/#roboto-font
+      <Html>
+        <Head>
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
