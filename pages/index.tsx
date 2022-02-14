30c47465198e87e230676f513f7870d8b8d1748d
Sergey Stadnik <sergey@snowytime.net>
2022-02-15 02:36:38 +1100

    feat: introcude MUI component library
    Ref: https://mui.com/

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,10 @@
+import Container from '@mui/material/Container';
 import { StocksGrid } from 'components/StocksGrid';
 
-const IndexPage: React.FunctionComponent = () => <StocksGrid />;
+const IndexPage: React.FunctionComponent = () => (
+  <Container maxWidth="lg">
+    <StocksGrid />
+  </Container>
+);
 
 export default IndexPage;
