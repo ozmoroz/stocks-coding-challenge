b5f0225a3f54617ace4e47e68c53870f6f0743ff
Sergey Stadnik <sergey@snowytime.net>
2022-02-15 18:05:14 +1100

    fix: style skeleton tile border to look more like the real thing

--- components/SkeletonTile/styles.tsx
+++ components/SkeletonTile/styles.tsx
@@ -3,6 +3,7 @@ import ContentLoader from 'react-content-loader';
 
 export const StyledContentLoader = styled(ContentLoader)`
   border: 1px solid #ecebeb;
+  border-radius: 4px;
   height: 20rem;
   padding: 2rem;
 `;
